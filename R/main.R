cb9861bc6fb79349bc5a76d87f19ce3a5cc1a877
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-09-06 19:56:00 +0100

    Add R/main.R

---
+++ R/main.R
@@ -0,0 +1,12 @@
+# Title     : main.R
+# Objective : main
+# Created by: greyhypotheses
+# Created on: 06/09/2022
+
+
+# import the data in focus
+source(file = 'R/import.R')
+
+
+# calculate correlations
+source(file = 'R/correlation/interface.R')
